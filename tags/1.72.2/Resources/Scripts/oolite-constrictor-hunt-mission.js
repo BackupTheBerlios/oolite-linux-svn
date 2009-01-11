d44cbbe6a986b6840a1f81a53e0c7c502d0062d4
ahruman <ahruman@127b21dd-08f5-0310-b4b7-95ae10353056>
2009-01-11 15:18:44 +0000

    Tagged 1.72.2.

---
+++ tags/1.72.2/Resources/Scripts/oolite-constrictor-hunt-mission.js
@@ -0,0 +1,149 @@
+/*
+
+oolite-constrictor-hunt-mission.js
+
+Script for Constrictor hunt mission.
+
+
+Oolite
+Copyright © 2004-2008 Giles C Williams and contributors
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+MA 02110-1301, USA.
+
+*/
+
+
+this.name			= "oolite-constrictor-hunt";
+this.author			= "eric walch";
+this.copyright		= "© 2008 the Oolite team.";
+this.version		= "1.72.2";
+
+
+this.addToScreen = function ()
+{
+	if (guiScreen == "GUI_SCREEN_SYSTEM_DATA")
+	{
+		if (galaxyNumber == 0)
+		{
+			switch (system.ID)
+			{
+				case 28:
+				case 36:
+				case 150:
+					mission.addMessageTextKey("constrictor_hunt_0_" + system.ID);
+					break;
+					
+				default:
+					break;
+			}
+		}
+		if (galaxyNumber == 1)
+		{
+			switch (system.ID)
+			{
+				case 3:
+				case 5:
+				case 16:
+				case 26:
+				case 32:
+				case 68:
+				case 106:
+				case 107:
+				case 162:
+				case 164:
+				case 184:
+				case 192:
+				case 220:
+					mission.addMessageTextKey("constrictor_hunt_1_A");
+					break;
+					
+				case 253:
+				case 79:
+				case 53:
+				case 118:
+				case 193:
+					mission.addMessageTextKey("constrictor_hunt_1_" + system.ID);
+					break;
+					
+				default:
+					break;
+			}
+		}
+	}
+}
+
+
+this.missionOffers = function ()
+{
+	if (guiScreen == "GUI_SCREEN_MISSION" || (mission.choice && mission.choice != "") || !player.ship.docked)  return;
+	
+	// there will be a "missionScreenEnded" or a "missionChoiceWasReset" in future to react to.
+	if (player.ship.dockedStation.isMainStation)
+	{
+		if (galaxyNumber < 2 && !missionVariables.conhunt && player.score > 255)
+		{
+			mission.runMissionScreen("constrictor_hunt_brief1", null, null, "constrictor");
+			if (galaxyNumber == 0)  mission.addMessageTextKey("constrictor_hunt_brief1a"); // galaxy = 0
+			else  mission.addMessageTextKey("constrictor_hunt_brief1b"); // galaxy = 1
+			missionVariables.conhunt = "STAGE_1";
+			mission.setInstructionsKey("conhunt_short_desc1");
+		}
+		if (missionVariables.conhunt == "CONSTRICTOR_DESTROYED")  // Variable is set by the ship-script
+		{
+			mission.runMissionScreen("constrictor_hunt_debrief", null, null, "constrictor");
+			player.credits += 5000;
+			player.bounty = 0;	  // legal status
+			player.score += 256;  // ship kills
+			mission.setInstructionsKey(null);  // reset the missionbriefing
+			missionVariables.conhunt = "MISSION_COMPLETE";
+		}
+	}
+}
+
+this.setUpShips = function ()
+{
+	if (galaxyNumber == 1 &&
+		system.ID == 193 &&
+		missionVariables.conhunt == "STAGE_1" &&
+		system.countShipsWithRole("constrictor") == 0)
+	{
+		system.legacy_addShips("constrictor", 1);
+	}
+}
+
+
+/**** Event handlers ****/
+
+this.guiScreenChanged = function ()
+{
+	if (galaxyNumber < 2 && missionVariables.conhunt == "STAGE_1")  this.addToScreen();
+}
+
+this.shipDockedWithStation = function ()
+{
+	this.missionOffers();
+}
+
+this.missionScreenEnded = this.missionChoiceWasReset = function ()
+{
+	if (!player.ship.docked) return;
+	this.missionOffers();
+}
+
+this.shipExitedWitchspace = this.shipLaunchedFromStation = function ()
+{
+	this.setUpShips();
+}
---
+++ tags/1.72.2/Resources/Scripts/oolite-default-ship-script.js
@@ -0,0 +1,108 @@
+/*
+
+oolite-default-ship-script.js
+
+Standard ship script; handles legacy foo_actions.
+
+
+Oolite
+Copyright © 2004-2008 Giles C Williams and contributors
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+MA 02110-1301, USA.
+
+*/
+
+
+this.name			= "oolite-default-ship-script";
+this.author			= "Jens Ayton";
+this.copyright		= "© 2007-2008 the Oolite team.";
+this.description	= "Standard script for ships.";
+this.version		= "1.72.2";
+
+
+// launch_actions handled on didSpawn().
+if (this.legacy_launchActions != undefined)
+{
+	this.shipSpawned = function ()
+	{
+		/*	IMPORTANT: runLegacyScriptActions() is a private function. It may
+			be removed, renamed or have its semantics changed at any time in
+			the future. Do not use it in your own scripts.
+		*/
+		this.ship.runLegacyScriptActions(this.ship, this.legacy_launchActions);
+		
+		// These can only be used once; keeping them around after that is pointless.
+		delete this.didSpawn;
+		delete this.legacy_launchActions;
+	}
+}
+
+
+// death_actions handled on shipDied().
+if (this.legacy_deathActions != undefined)
+{
+	this.shipDied = function ()
+	{
+		/*	IMPORTANT: runLegacyScriptActions() is a private function. It may
+			be removed, renamed or have its semantics changed at any time in
+			the future. Do not use it in your own scripts.
+		*/
+		this.ship.runLegacyScriptActions(this.ship, this.legacy_deathActions);
+	}
+}
+
+
+// script_actions handled on shipDidDock() and wasScooped().
+if (this.legacy_scriptActions != undefined)
+{
+	/*	legacy script_actions should be called for stations when the player
+		docks, and for cargo pods when they are is scooped. No sane vessel can
+		be scooped _and_ docked with. Non-sane vessels are certified insane.
+	*/
+	this.otherShipDocked = function (docker)
+	{
+		if (docker.isPlayer)
+		{
+			/*	IMPORTANT: runLegacyScriptActions() is a private function. It
+				may be removed, renamed or have its semantics changed at any
+				time in the future. Do not use it in your own scripts.
+			*/
+			this.ship.runLegacyScriptActions(docker, this.legacy_scriptActions);
+		}
+	}
+	this.shipWasScooped = function (scooper)
+	{
+		/*	IMPORTANT: runLegacyScriptActions() is a private function. It may
+			be removed, renamed or have its semantics changed at any time in
+			the future. Do not use it in your own scripts.
+		*/
+		
+		// Note "backwards" call, allowing awardEquipment: and similar to affect the scooper rather than the scoopee.
+		scooper.runLegacyScriptActions(this.ship, this.legacy_scriptActions);
+	}
+}
+
+
+// setup_actions handled on script initialization.
+if (this.legacy_setupActions != undefined)
+{
+	/*	IMPORTANT: runLegacyScriptActions() is a private function. It may be
+		removed, renamed or have its semantics changed at any time in the
+		future. Do not use it in your own scripts.
+	*/
+	this.ship.runLegacyScriptActions(this.ship, this.legacy_setupActions);
+	delete this.legacy_setupActions;
+}
